a5cfc1779bee6af7be58f6aaa80ed64bd48f799d
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-08-27 17:22:49 +0500

    Fix: Update application URL to production and streamline database configuration

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 
 
-define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
-define('DB_PORT', getenv('DB_PORT') ?: '3306');
-define('DB_DATABASE', getenv('DB_DATABASE') ?: 'osontaklif');
-define('DB_USERNAME', getenv('DB_USERNAME') ?: 'webadmin');
-define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'root');
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_PORT', getenv('DB_PORT'));
+define('DB_DATABASE', getenv('DB_DATABASE'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 class Database
 {
